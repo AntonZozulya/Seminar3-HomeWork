0af3a9b4834b2ff6dcdb16890598e23b8f284993
AntonZozulya <antonzo93@mail.ru>
2023-03-22 20:28:47 +0300

    задача 23

--- S3Homework_task_23/Program.cs
+++ S3Homework_task_23/Program.cs
@@ -2,28 +2,8 @@
 // 3 -> 1, 8, 27
 // 5 -> 1, 8, 27, 64, 125
 
-Console.WriteLine("\nЗадача 23");
+Console.Clear();
 Console.Write("Введите число: ");
-int cube = Convert.ToInt32(Console.ReadLine());
-
-void Cube(int[] cube){
-  int counter = 0;
-  int length = cube.Length;
-  while (counter <  length){
-    cube[counter] = Convert.ToInt32(Math.Pow(counter, 3));
-    counter++;
-  }
-}
-
-void PrintArry(int[] coll){
-  int count = coll.Length;
-  int index = 0;
-  while(index < count){
-    Console.Write(coll[index]+ " ");
-    index++;
-  }
-} 
-
-int[] arry = new int[cube+1];
-Cube(arry);
-PrintArry(arry);
+int n = int.Parse(Console.ReadLine()!);
+for (int i = 1; i <= n; i++)
+  Console.WriteLine($"{i} ^ 3 = {i * i * i}");
